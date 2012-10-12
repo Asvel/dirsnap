63051600b06695caa609db6aced0200e1cd262a1
Asvel <fe.asvel@gmail.com>
2012-10-12 23:35:18 +0800

    删除功能初步完成

--- process.py
+++ process.py
@@ -4,6 +4,14 @@ import snapshot
 import re
 import os
 
+def delete(obj, path):
+    path_part = path[len(obj['from']) + 1:].split(os.sep)
+    item = obj
+    for part in path_part[:-1]:
+        item = item['item'][part]
+    del item['item'][path_part[len(path_part) - 1]]
+
+
 def main():
     filename = r"C:\project\OfflineDirectory\test.html";
     with open(filename, 'r', encoding='utf-8') as fp:
@@ -15,5 +23,17 @@ def main():
     dirlist = [split.match(x).group(1) for x in dirlist]
     print('\n'.join(dirlist))
 
+def test():
+    filename = r"C:\project\OfflineDirectory\test.html";
+    with open(filename, 'r', encoding='utf-8') as fp:
+        dirobj = snapshot.load_html(fp.read())
+    delete(dirobj, r'M:\anime_chk\air')
+    filename = r"C:\project\OfflineDirectory\test1.html";
+    with open(r"C:\project\OfflineDirectory\viewer.html", 'r', encoding='utf-8') as fp:
+        template = fp.read()
+    with open(filename, 'w', encoding='utf-8') as fp:
+        fp.write(snapshot.dump_html(dirobj, template))
+
 if __name__ == '__main__':
-    main()
+    #main()
+    test()
