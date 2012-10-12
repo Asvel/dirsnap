b8c38e46f8fdd5e044b9a68d7451c9bf75c296e1
Asvel <fe.asvel@gmail.com>
2012-10-12 23:24:32 +0800

    规范代码

--- snapshot.py
+++ snapshot.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 import time
@@ -8,8 +10,8 @@ _html_json_begin_mark = '/* OfflineDirectory JSON Begin */'
 _html_json_end_mark = '/* OfflineDirectory JSON End */'
 
 def _calc_dir_info(obj):
-    '''计算目录属性
-    '''
+    """计算目录属性
+    """
     for item in obj['item'].values():
         if 'item' in item:
             _calc_dir_info(item)
@@ -17,10 +19,10 @@ def _calc_dir_info(obj):
     obj['time'] = max([x['time'] for x in obj['item'].values()] + [0])
 
 def load_dir(path):
-    '''抓取目录列表为一个字典
-    
+    """抓取目录列表为一个字典
+
     path 要抓取的目录
-    
+
     返回值的结构：
     {
         "from": <抓取的目录>
@@ -36,22 +38,22 @@ def load_dir(path):
                 "time": <该目录的子项目的最后修改时间>
                 "item"：{
                     <子项目，格式同上，递归结构>
-                } 
+                }
             }
             ...
         }
     }
-    '''
+    """
 
     def load_a_dir(path):
-        '''递归抓取目录
-        '''
+        """递归抓取目录
+        """
 
         # 获取子项目列表
         try:
             subitem_name = os.listdir(path)
         except:
-            print('获取此目录的子项目时发生异常：', path.strip('\\?'))
+            print("获取此目录的子项目时发生异常：", path.strip('\\?'))
             return {'item': {}}
 
         # 获取子项目属性
@@ -65,7 +67,7 @@ def load_dir(path):
                     newitem['size'] = info.st_size
                     newitem['time'] = int(info.st_mtime * 1000)
                 except:
-                    print('获取此文件的属性时发生异常：', newpath.strip('\\?'))
+                    print("获取此文件的属性时发生异常：", newpath.strip('\\?'))
             else:
                 newitem = load_a_dir(newpath)
             subitem[x] = newitem
@@ -83,21 +85,21 @@ def load_dir(path):
     return result
 
 def load_json(s):
-    '''读取 JSON 格式字符串 s
-    
+    """读取 JSON 格式字符串 s
+
     s 包含 JSON 的字符串
     返回读取到的对象
-    '''
+    """
     obj = json.loads(s)
     _calc_dir_info(obj)
     return obj
 
 def load_html(s):
-    '''从 HTML 字符串 s 中读取 JSON 格式字符串
-    
+    """从 HTML 字符串 s 中读取 JSON 格式字符串
+
     s 包含 JSON 字符串的 HTML 字符串
     返回读取到的对象
-    '''
+    """
     global _html_json_begin_mark
     global _html_json_end_mark
     begi = s.index(_html_json_begin_mark) + len(_html_json_begin_mark)
@@ -105,14 +107,14 @@ def load_html(s):
     return load_json(s[begi:endi])
 
 def dump_json(obj):
-    '''由对象 obj 生成 JSON 格式的字符串
-    
+    """由对象 obj 生成 JSON 格式的字符串
+
     obj 作为数据源的对象
     返回生成的 JSON 格式字符串
-    '''
+    """
     def rename_key_for_sort(obj):
-        '''修改键名用于排序（子项目排在最后）
-        '''
+        """修改键名用于排序（子项目排在最后）
+        """
         for x in obj['item'].values():
             if 'item' in x:
                 rename_key_for_sort(x)
@@ -129,33 +131,33 @@ def dump_json(obj):
     ).replace('|', '')
 
 def dump_html(obj, template):
-    '''由对象 obj 生成 HTML 格式的字符串
-    
+    """由对象 obj 生成 HTML 格式的字符串
+
     obj 作为数据源的对象
     template HTML 模板
     返回生成的 HTML 格式字符串
-    '''
+    """
     jsons = dump_json(obj)
     global _html_json_end_mark
     i = template.index(_html_json_end_mark)
     return template[:i] + jsons + '\n' + template[i:];
 
 def _filename_sort_key(s):
-    '''返回排序文件名的键值
-    '''
+    """返回排序文件名的键值
+    """
     return locale.strxfrm(s.lower())
 
 def dump_tree(obj, indent='\t'):
-    '''由对象 obj 生成树形表示的字符串
-    
+    """由对象 obj 生成树形表示的字符串
+
     obj 作为数据源的对象，应符合 load_dir() 的格式
     indent 缩进使用的字符串
-    '''
+    """
     locale.setlocale(locale.LC_COLLATE, '')
 
     def dump_tree_dir(obj, depth):
-        '''递归生成树形
-        '''
+        """递归生成树形
+        """
         for name in sorted(obj['item'].keys(), key=_filename_sort_key):
             desc.append(indent * (depth + 1) + name)
             if 'item' in obj['item'][name]:
@@ -166,15 +168,15 @@ def dump_tree(obj, indent='\t'):
     return '\n'.join(desc)
 
 def dump_list(obj):
-    '''由对象 obj 生成列表表示的字符串
-    
+    """由对象 obj 生成列表表示的字符串
+
     obj 作为数据源的对象，应符合 load_dir() 的格式
-    '''
+    """
     locale.setlocale(locale.LC_COLLATE, '')
 
     def dump_list_dir(obj, path):
-        '''递归生成列表
-        '''
+        """递归生成列表
+        """
         for name in sorted(obj['item'].keys(), key=_filename_sort_key):
             fullname = os.path.join(path, name)
             desc.append(fullname)
@@ -230,24 +232,24 @@ def main():
     args = parser.parse_args()
 
     def make_longunc(path):
-        '''生成长 UNC 路径
-        
+        """生成长 UNC 路径
+
         path 要生成长 UNC 路径的路径
         返回 path 的长 UNC 形式
         如果 path 本来就是长 UNC 路径则原样返回
-        '''
+        """
         if (os.path.__name__ == 'ntpath') and not path.startswith('\\\\?\\'):
             path = '\\\\?\\' + path
         return path
 
     # 准备路径信息
     frompath = make_longunc(os.path.abspath(args.frompath + os.sep))
-    if args.fromtype == None:
+    if args.fromtype is None:
         if os.path.isdir(frompath):
             args.fromtype = 'dir'
         else:
             args.fromtype = 'html'
-    if args.topath == None:
+    if args.topath is None:
         if args.fromtype == 'dir':
             trans = str.maketrans({x: '_' for x in '\\/:*?"<>|'})
             topath = frompath.strip(':\\?').translate(trans)
