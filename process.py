80c532d105553dd73e410acc9e28f8dcfd6128fb
Asvel <fe.asvel@gmail.com>
2012-10-19 20:04:51 +0800

    微调

--- process.py
+++ process.py
@@ -4,16 +4,17 @@ import snapshot
 import re
 import os
 
+#def dump_list_depth()
+
 def delete(obj, path):
     path_part = path[len(obj['from']) + 1:].split(os.sep)
     item = obj
     for part in path_part[:-1]:
         item = item['item'][part]
-    del item['item'][path_part[len(path_part) - 1]]
-
+    del item['item'][path_part[-1]]
 
 def main():
-    filename = r"C:\project\OfflineDirectory\test.html"
+    filename = "test.html"
     with open(filename, 'r', encoding='utf-8') as fp:
         dirobj = snapshot.load_html(fp.read())
     exp = re.compile('^' + '[^\{0}]*?\{0}'.format(os.sep) * 4 + '$')
@@ -24,15 +25,13 @@ def main():
     print('\n'.join(dirlist))
 
 def test():
-    filename = r"C:\project\OfflineDirectory\test.html"
-    with open(filename, 'r', encoding='utf-8') as fp:
+    with open("test.html", 'r', encoding='utf-8') as fp:
         dirobj = snapshot.load_html(fp.read())
+
     delete(dirobj, r'M:\anime_chk\air')
-    filename = r"C:\project\OfflineDirectory\test1.html"
-    with open(r"C:\project\OfflineDirectory\viewer.html", 'r', encoding='utf-8') as fp:
-        template = fp.read()
-    with open(filename, 'w', encoding='utf-8') as fp:
-        fp.write(snapshot.dump_html(dirobj, template))
+
+    with open("test1.html", 'w', encoding='utf-8') as fp:
+        fp.write(snapshot.dump_html(dirobj))
 
 if __name__ == '__main__':
     #main()
