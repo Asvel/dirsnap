b5dd41e692613ba2bebe9f76771aa6b6d10b5a00
Asvel <fe.asvel@gmail.com>
2012-10-19 20:33:46 +0800

    添加生成指定深度项目的列表的功能

--- process.py
+++ process.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import snapshot
-import re
 import os
 
-#def dump_list_depth()
+def dump_list_depth(obj, depth):
+    depth += obj['from'].strip(os.sep).count(os.sep)
+    list = snapshot.dump_list(obj)
+    list = [x for x in list if x.strip(os.sep).count(os.sep) == depth]
+    return list
 
 def delete(obj, path):
     path_part = path[len(obj['from']) + 1:].split(os.sep)
@@ -13,26 +16,16 @@ def delete(obj, path):
         item = item['item'][part]
     del item['item'][path_part[-1]]
 
-def main():
-    filename = "test.html"
-    with open(filename, 'r', encoding='utf-8') as fp:
-        dirobj = snapshot.load_html(fp.read())
-    exp = re.compile('^' + '[^\{0}]*?\{0}'.format(os.sep) * 4 + '$')
-    dirlist = snapshot.dump_list(dirobj)
-    dirlist = [x for x in dirlist if exp.match(x) is not None]
-    split = re.compile('^.*\{0}(.*)\{0}$'.format(os.sep))
-    dirlist = [split.match(x).group(1) for x in dirlist]
-    print('\n'.join(dirlist))
-
 def test():
     with open("test.html", 'r', encoding='utf-8') as fp:
         dirobj = snapshot.load_html(fp.read())
 
-    delete(dirobj, r'M:\anime_chk\air')
+    #delete(dirobj, r'M:\anime_chk\air')
+    #output = snapshot.dump_html(dirobj)
+    output = "\n".join(dump_list_depth(dirobj, 2))
 
     with open("test1.html", 'w', encoding='utf-8') as fp:
-        fp.write(snapshot.dump_html(dirobj))
+        fp.write(output)
 
 if __name__ == '__main__':
-    #main()
     test()
